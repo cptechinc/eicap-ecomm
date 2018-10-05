4aebd5807bdcfaf5ba953883925814092681a936
bbullemer <barbara.bullemer@gmail.com>
2018-10-05 14:17:53 -0500

    Homepage Redesign
    - moved user-page elements to the home page so users will see options immediately on login

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,38 +1,41 @@
 <?php include('./_head.php'); // include header markup ?>
 
-<div class="jumbotron page-banner" style="background: url('<?= $page->pagebanner->url; ?>'); background-size: cover;"></div>
 	<div class="container page">
-		<h1 class="text-danger font-weight-bold">Welcome back, <?= $user->fullname; ?>!</h1>
-		<div class="row mt-4">
-			<a href="#" class="col-sm-4">
-				<div class="card bg-primary">
-				    <div class="card-body">
-				    	<h5 class="card-title text-center"><i class="fa fa-cart-plus fa-5x text-white" aria-hidden="true"></i></h5>
-				    	<p class="card-text text-center text-white">Create New Order</p>
-				  	</div>
-				</div>
-			</a>
-			<a href="<?= $pages->get('/user/orders/')->url; ?>" class="col-sm-4">
-				<div class="card bg-primary">
-				    <div class="card-body">
-				    	<h5 class="card-title text-center"><i class="fa fa-list-alt fa-5x text-white" aria-hidden="true"></i></h5>
-				    	<p class="card-text text-center text-white">Past Orders</p>
-				  	</div>
-				</div>
-			</a>
-			<a href="<?= $pages->get('/user/')->url; ?>" class="col-sm-4">
-				<div class="card bg-primary">
-				    <div class="card-body">
-				    	<h5 class="card-title text-center"><i class="fa fa-user fa-5x text-white" aria-hidden="true"></i></h5>
-				    	<p class="card-text text-center text-white">Your Account</p>
-				  	</div>
-				</div>
-			</a>
-		</div>
-		<div class="row mt-4">
+		<div class="row mt-5 mb-1">
 			<div class="col-sm-12">
-				<h3 class="text-danger font-weight-bold"><?= $page->summary; ?></h3>
-				<p><?= $page->body; ?></p>
+				<h1 class="font-weight-bold text-danger">Welcome back, <?= $user->fullname; ?>!</h1>
+			</div>
+		</div>
+		<div class="row bg-primary rounded">
+			<div class="col-sm-4 my-3">
+				<div class="card bg-white">
+					<a href="<?= $pages->get('/user/account/')->url; ?>">
+						<div class="card-body">
+							<h1 class="text-center text-danger"><i class="fa fa-address-card fa-2x" aria-hidden="true"></i></h1>
+							<p class="card-text font-weight-bold text-center text-danger">Profile</p>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="col-sm-4 my-3">
+				<div class="card bg-white">
+					<a href="<?= $pages->get('/user/orders/')->url; ?>">
+						<div class="card-body">
+							<h1 class="text-center text-danger"><i class="fa fa-list-alt fa-2x" aria-hidden="true"></i></h1>
+							<p class="card-text font-weight-bold text-center text-danger">Past Orders</p>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="col-sm-4 my-3">
+				<div class="card bg-white">
+					<a href="#">
+						<div class="card-body">
+							<h1 class="text-center text-danger"><i class="fa fa-map-marker fa-2x" aria-hidden="true"></i></h1>
+							<p class="card-text font-weight-bold text-center text-danger">Addresses</p>
+						</div>
+					</a>
+				</div>
 			</div>
 		</div>
 	</div>
