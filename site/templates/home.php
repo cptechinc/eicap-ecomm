45ebf2a5cd389c54b04553653688a942407b1b7a
Paul Gomez <pauldro952@outlook.com>
2018-10-05 12:34:49 -0500

    Orders classes

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,7 +1,6 @@
 <?php include('./_head.php'); // include header markup ?>
 
-<div class="jumbotron page-banner" style="background: url('<?= $page->pagebanner->url; ?>'); background-size: cover;">
-</div>
+<div class="jumbotron page-banner" style="background: url('<?= $page->pagebanner->url; ?>'); background-size: cover;"></div>
 	<div class="container page">
 		<h1 class="text-danger font-weight-bold">Welcome back, <?= $user->fullname; ?>!</h1>
 		<div class="row mt-4">
@@ -13,7 +12,7 @@
 				  	</div>
 				</div>
 			</a>
-			<a href="#" class="col-sm-4">
+			<a href="<?= $pages->get('/user/orders/')->url; ?>" class="col-sm-4">
 				<div class="card bg-primary">
 				    <div class="card-body">
 				    	<h5 class="card-title text-center"><i class="fa fa-list-alt fa-5x text-white" aria-hidden="true"></i></h5>
