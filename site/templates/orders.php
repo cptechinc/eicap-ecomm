e538eaaaf69fdc72d729b9f99c981d77207e1f7f
bbullemer <barbara.bullemer@gmail.com>
2018-10-07 22:24:46 -0500

    Orders Page
    - added functions to pull in orders to table
    - added pagination to table
    *** using sessionid from MySQL table because no sessionid was found in mysql for my current one

--- site/templates/orders.php
+++ site/templates/orders.php
@@ -1,14 +1,15 @@
-<?php 
-	$salesordersdisplay = new SalesOrdersDisplay(session_id(), $page->fullURL, $modal = '', $loadint = '', $ajax = false);
+<?php
+	$salesordersdisplay = new SalesOrdersDisplay('1tutf4nrj4k3mh7torcjbd8e7t', $page->fullURL, $modal = '', $loadint = '', $ajax = false);
 	$salesordersdisplay->pagenbr = $input->pageNum;
 	$salesordersdisplay->get_ordercount();
-	$salesordersdisplay->paginationinsertafter = $page->name; 
+	$salesordersdisplay->paginationinsertafter = $page->name;
 	$paginator = new Paginator($salesordersdisplay->pagenbr, $salesordersdisplay->count, $salesordersdisplay->pageurl->getUrl(), $salesordersdisplay->paginationinsertafter, $salesordersdisplay->ajaxdata);
 ?>
+
 <?php $orders = $salesordersdisplay->get_orders(); ?>
 <?php include('./_head.php'); // include header markup ?>
 	<div class="container page top-margin">
-		<h1 class="text-danger font-weight-bold">Your Orders</h1>
+		<h1 class="text-danger font-weight-bold border-bottom border-primary">Your Orders</h1>
         <div class="list-group">
 			<div class="form-group">
 				<div href="#" class="list-group-item list-group-item-action bg-secondary text-white font-weight-bold">
@@ -32,38 +33,11 @@
 	                </a>
 	            <?php endforeach; ?>
 			</div>
-			
+
 			<div class="align-self-center"><?= $paginator; ?></div>
-			
-			<a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-				<div class="d-flex w-100 justify-content-between">
-					<h5 class="mb-1"><?= $order->custid; ?></h5>
-					<small class="text-muted"><?= $order->orderno; ?></small>
-				</div>
-				<p class="mb-1"><?= $order->custpo; ?></p>
-				<small class="text-muted">$<?= $order->ordertotal; ?></small>
-			</a>
         </div>
-		<table class="table table-striped">
-			<thead>
-				<tr>
-					<th scope="col">#</th>
-					<th scope="col">Customer</th>
-					<th scope="col">Ship to</th>
-					<th scope="col">Order Total</th>
-					<th scope="col">Order Date</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<th scope="row"><?= $order->orderno; ?></th>
-					<td><?= $order->custid; ?></td>
-					<td><?= $order->shiptoid; ?></td>
-					<td>$ <?= $order->ordertotal; ?></td>
-					<td><?= $order->orderdate; ?></td>
-				</tr>
-			</tbody>
-		</table>
+		<a href="<?= $pages->get('/')->url; ?>" class="btn btn-primary my-1">Go back to Account Page</a>
+
     </div>
 	<!-- end content -->
 <?php include('./_foot.php'); // include footer markup ?>
--- vendor/cptechinc/dplus-ecomm/src/SalesOrdersDisplay.class.php
+++ vendor/cptechinc/dplus-ecomm/src/SalesOrdersDisplay.class.php
@@ -1,12 +1,12 @@
-<?php 	
+<?php
 	class SalesOrdersDisplay extends SalesOrderPanel  {
-		
+
         public function __construct($sessionID, \Purl\Url $pageurl, $modal, $loadinto, $ajax) {
 			parent::__construct($sessionID, $pageurl, $modal, $loadinto, $ajax);
 			$this->pageurl = new Purl\Url($pageurl->getUrl());
 			$this->setup_pageurl();
 		}
-        
+
         /* =============================================================
 			OrderPanelInterface Functions
 			LINKS ARE HTML LINKS, AND URLS ARE THE URLS THAT THE HREF VALUE
@@ -14,11 +14,31 @@
 		public function setup_pageurl() {
 			$this->pageurl->query->remove('display');
 		}
-		
+
 		public function generate_loaddetailsurl(Order $order) {
 			$url = new \Purl\Url(DplusWire::wire('pages')->get('/user/orders/redir/')->url);
 			$url->query->set('action', 'get-order-details');
 			$url->query->set('ordn', $order->orderno);
 			return $url->getUrl();
+		}
+
+
+		/* =============================================================
+			Class Functions
+		============================================================ */
+		public function get_order($debug = false) {
+			return get_orderhead($this->sessionID, $this->ordn, $debug);
+		}
+
+		public function get_orders($debug = false) {
+			$useclass = true;
+			$this->tablesorter->sortrule = 'DESC';
+			$orders = get_userordersorderdate($this->sessionID, DplusWire::wire('session')->display, $this->pagenbr, $this->tablesorter->sortrule, $this->filters, $this->filterable, $useclass, $debug);
+			return $debug ? $orders : $this->orders = $orders;
+		}
+
+		public function get_ordercount($debug = false) {
+			$count = count_userorders($this->sessionID, $this->filters, $this->filterable, $debug);
+			return $debug ? $count : $this->count = $count;
 		}
 	}
