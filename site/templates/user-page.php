02d20e1a86123e60f1b56baa109493fbf8b62255
bbullemer <barbara.bullemer@gmail.com>
2018-10-09 09:24:13 -0500

    Small Design changes
    -small design changes

--- site/templates/user-page.php
+++ site/templates/user-page.php
@@ -1,9 +1,9 @@
-<?php include('./_head.php'); ?>
+<?php include('./_head.php'); // include header markup ?>
 
-	<div class='container page'>
+	<div class="container page">
 		<div class="row mt-5 mb-1">
 			<div class="col-sm-12">
-				<h1 class="font-weight-bold text-danger">Your Account</h1>
+				<h1 class="font-weight-bold text-danger">Welcome back, <?= $user->fullname; ?>!</h1>
 			</div>
 		</div>
 		<div class="row bg-primary rounded">
@@ -19,7 +19,7 @@
 			</div>
 			<div class="col-sm-4 mt-3">
 				<div class="card bg-white">
-					<a href="#">
+					<a href="<?= $pages->get('/user/orders/')->url; ?>">
 						<div class="card-body">
 							<h1 class="text-center text-danger"><i class="fa fa-list-alt fa-2x" aria-hidden="true"></i></h1>
 							<p class="card-text font-weight-bold text-center text-danger">Past Orders</p>
@@ -29,20 +29,30 @@
 			</div>
 			<div class="col-sm-4 mt-3">
 				<div class="card bg-white">
-					<a href="#">
+					<a href="<?= $pages->get('/cart/')->url; ?>">
 						<div class="card-body">
-							<h1 class="text-center text-danger"><i class="fa fa-map-marker fa-2x" aria-hidden="true"></i></h1>
-							<p class="card-text font-weight-bold text-center text-danger">Addresses</p>
+							<h1 class="text-center text-danger"><i class="fa fa-shopping-cart fa-2x" aria-hidden="true"></i></h1>
+							<p class="card-text font-weight-bold text-center text-danger">Cart</p>
 						</div>
 					</a>
 				</div>
 			</div>
-			<div class="col-sm-4 mt-3 mb-3">
+			<div class="col-sm-4 my-3">
 				<div class="card bg-white">
-					<a href="#">
+					<a href="<?= $pages->get('/user/add-vendor/')->url; ?>">
 						<div class="card-body">
-							<h1 class="text-center text-danger"><i class="fa fa-credit-card fa-2x" aria-hidden="true"></i></h1>
-							<p class="card-text font-weight-bold text-center text-danger">Payments</p>
+							<h1 class="text-center text-danger"><i class="fa fa-user-plus fa-2x" aria-hidden="true"></i></h1>
+							<p class="card-text font-weight-bold text-center text-danger">Add New Vendor</p>
+						</div>
+					</a>
+				</div>
+			</div>
+			<div class="col-sm-4 my-3">
+				<div class="card bg-white">
+					<a href="<?= $pages->get('/user/add-item/')->url; ?>">
+						<div class="card-body">
+							<h1 class="text-center text-danger"><i class="fa fa-plus-circle fa-2x" aria-hidden="true"></i></h1>
+							<p class="card-text font-weight-bold text-center text-danger">Add New Item</p>
 						</div>
 					</a>
 				</div>
@@ -51,4 +61,4 @@
 	</div>
 	<!-- end content -->
 
-<?php include('./_foot.php'); ?>
+<?php include('./_foot.php'); // include footer markup ?>
