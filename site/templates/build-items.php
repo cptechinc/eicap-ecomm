f484f1670ae2e1e8c8f7e4bee46ae405e1989bbd
Paul Gomez <pauldro952@outlook.com>
2018-11-12 08:46:46 -0600

    Build Items also builds dplus users

--- site/templates/build-items.php
+++ site/templates/build-items.php
@@ -1,7 +1,14 @@
 <?php 
+    use Dplus\Ecomm\Pages\ItemGroupPages;
+    use Dplus\Ecomm\Pages\ItemMasterItemPages;
+    
     header('Content-Type: application/json');
+    $itemgroups = new ItemGroupPages();
+    $items = new ItemMasterItemPages;
+    
     $results = array(
-        'item-groups' => ItemGroup::import_itemgroups(),
-        'items' => ItemMasterItem::import_items()
+        'item-groups' => $itemgroups->import_all(),
+        'items'       => $items->import_all(),
+        'users'       => import_logmintoprocesswire()
     );
     echo json_encode($results);
