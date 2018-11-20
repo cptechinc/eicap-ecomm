ecd8ecec749e68b06d22e81ca2ce1008a1644bc6
bbullemer <barbara.bullemer@gmail.com>
2018-11-20 11:10:11 -0600

    Products Search
    - when typing a few numbers from product IDs, no results were showing up because search was looking for exact match, added % to look if it was included inside IDs

--- site/templates/products-search.php
+++ site/templates/products-search.php
@@ -1,14 +1,14 @@
-<?php 
+<?php
 	$q = $input->get->text('q');
 	$page->title = $input->get->q ? "Searching for '$q'" : "Search Products";
-	$products = $pages->find("template=product|imitem, title|body|itemid|name1|name2*=$q");
+	$products = $pages->find("template=product|imitem, title|body|itemid|name1|name2%=$q");
 
 	if ($config->ajax) {
 		$page->body = $config->paths->content."products/search/results-ajax.php";
 		if ($config->modal) {
-			include('./_include-ajax-modal.php'); 
+			include('./_include-ajax-modal.php');
 		}
 	} else {
 		$page->body = $config->paths->content."products/search/results.php";
-		include('./_include-page.php'); 
+		include('./_include-page.php');
 	}
