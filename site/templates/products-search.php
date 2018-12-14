480d2418cad4dc89f968823361dd52d2005a7a26
bbullemer <barbara.bullemer@gmail.com>
2018-12-14 11:02:05 -0600

    EICAP fixes
    - removed empty field if statement for holdstatus so it would not be automatically checked
    - added % to search selector in products search to search entire string for match instead of exact match

--- site/templates/products-search.php
+++ site/templates/products-search.php
@@ -1,24 +1,26 @@
-<?php 
+<?php
 	$q = $input->get->text('q');
 	$page->title = $input->get->q ? "Searching for '$q'" : "Search Products";
-	
-	$selector = "template=product|imitem, title|body|itemid|name1|name2*=$q";
-	
+
+	// Processwire Selectors
+	// https://processwire.com/api/selectors/
+	$selector = "template=product|imitem, title|body|itemid|name1|name2%=$q";
+
 	if ($config->ajax) {
 		$limit = 10;
 		$start = $input->pageNum > 1 ? $input->pageNum * $limit : 0;
 		$selector .= ", limit=$limit, start=$start";
 	}
-	
+
 	$resultcount = $pages->count($selector);
 	$products = $pages->find($selector);
 
 	if ($config->ajax) {
 		$page->body = $config->paths->content."products/search/results-ajax.php";
 		if ($config->modal) {
-			include('./_include-ajax-modal.php'); 
+			include('./_include-ajax-modal.php');
 		}
 	} else {
 		$page->body = $config->paths->content."products/search/results.php";
-		include('./_include-page.php'); 
+		include('./_include-page.php');
 	}
