0ac93a9157914284127dd6aeabe0b117f23dc163
bbullemer <barbara.bullemer@gmail.com>
2019-02-18 14:03:43 -0600

    twig conversion
    - fixed products page spacing and title

--- site/templates/products.php
+++ site/templates/products.php
@@ -1,6 +1,5 @@
 <?php
-	$products = $page;
-	$item_categories = $products->children("template!=products-search");
+	$item_categories = $page->children("template!=products-search");
 
 	$search = $pages->get('template=products-search');
 
@@ -8,5 +7,5 @@
 		$ordn = $input->get->ordn;
 	}
 
-	$page->body = $config->twig->render('products/item-categories.twig', ['item_categories' => $item_categories, 'products' => $products, 'search' => $search, 'ordn' => $ordn]);
+	$page->body = $config->twig->render('products/item-categories.twig', ['page' => $page, 'item_categories' => $item_categories, 'search' => $search, 'ordn' => $ordn]);
 	include __DIR__ . "/basic-page.php";
