e958dffcb6fd29a63383560b426737aa58cde968
bbullemer <barbara.bullemer@gmail.com>
2019-02-18 11:29:10 -0600

    Products Search
    - created search twig file for products and included it in item-categories and item twig files

--- site/templates/item-group.php
+++ site/templates/item-group.php
@@ -2,5 +2,11 @@
 	$item_category = $page;
 	$items = $item_category->children();
 
-	$page->body =  $config->twig->render('products/item.twig', ['items' => $items, 'item_category' => $item_category]);
+	$search = $pages->get('template=products-search');
+
+	if ($input->get->ordn) {
+		$ordn = $input->get->ordn;
+	}
+
+	$page->body =  $config->twig->render('products/item.twig', ['items' => $items, 'item_category' => $item_category, 'search' => $search, 'ordn' => $ordn]);
 	include __DIR__ . "/basic-page.php";
--- site/templates/products.php
+++ site/templates/products.php
@@ -2,5 +2,11 @@
 	$products = $page;
 	$item_categories = $products->children("template!=products-search");
 
-	$page->body =  $config->twig->render('products/item-categories.twig', ['item_categories' => $item_categories, 'products' => $products]);
+	$search = $pages->get('template=products-search');
+
+	if ($input->get->ordn) {
+		$ordn = $input->get->ordn;
+	}
+
+	$page->body =  $config->twig->render('products/item-categories.twig', ['item_categories' => $item_categories, 'products' => $products, 'search' => $search, 'ordn' => $ordn]);
 	include __DIR__ . "/basic-page.php";
