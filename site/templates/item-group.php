3bc8a1152d8978ca58de0d45df7f95ca8dbeb71b
bbullemer <barbara.bullemer@gmail.com>
2019-02-26 10:52:33 -0600

    Products and Search Paginator
    - added pagination to products and search

--- site/templates/item-group.php
+++ site/templates/item-group.php
@@ -1,8 +1,8 @@
 <?php
+	use Dplus\Content\PaginatorBootstrap4;
+
 	$items = $page->children();
-
 	$search = $pages->get('template=products-search');
-
 	$formaction = "{$config->pages->cart}redir/";
 	$action = "add-to-cart";
 
@@ -10,5 +10,15 @@
 		$ordn = $input->get->ordn;
 	}
 
-	$page->body =  $config->twig->render('products/item.twig', ['items' => $items, 'page' => $page, 'page_title' => $page->title, 'search' => $search, 'ordn' => $ordn, 'formaction' => $formaction, 'action' => $action]);
+	$resultcount = $page->numChildren();
+
+	$limit = 10;
+	$pagenbr = $input->pageNum;
+	$start = $input->pageNum > 1 ? $input->pageNum * $limit : 0;
+
+	$totalpages = ceil($resultcount/$limit);
+
+	$paginator = new PaginatorBootstrap4($input->pageNum, $resultcount, $page->fullURL, $page->name);
+
+	$page->body = $config->twig->render('products/item.twig', ['items' => $items, 'page' => $page, 'page_title' => $page->title, 'search' => $search, 'ordn' => $ordn, 'formaction' => $formaction, 'action' => $action, 'paginator' => $paginator]);
 	include __DIR__ . "/basic-page.php";
--- site/templates/products-search.php
+++ site/templates/products-search.php
@@ -7,16 +7,12 @@
 	// Processwire Selectors
 	// https://processwire.com/api/selectors/
 	$selector = "template=product|imitem, title|body|itemid|name1|name2%=$q";
-
 	$search = $pages->get('template=products-search');
-
-	if ($config->ajax) {
-		$limit = 10;
-		$start = $input->pageNum > 1 ? $input->pageNum * $limit : 0;
-		$selector .= ", limit=$limit, start=$start";
-	}
-
+	$limit = 10;
+	$start = $input->pageNum > 1 ? $input->pageNum * $limit : 0;
+	$selector .= ", limit=$limit, start=$start";
 	$resultcount = $pages->count($selector);
+
 	$items = $pages->find($selector);
 
 	if ($input->get->ordn) {
@@ -25,7 +21,7 @@
 		$action = "add-to-order";
 	} else {
 		$formaction = "{$config->pages->cart}redir/";
-		$action = "add-to-order";
+		$action = "add-to-cart";
 	}
 
 	if ($config->ajax) {
@@ -40,6 +36,7 @@
 			include __DIR__ . "/_include-ajax-modal.php";
 		}
 	} else {
-		$page->body = $config->twig->render('products/item.twig', ['page' => $page, 'page_title' => $page->title, 'items' => $items, 'search' => $search, 'ordn' => $ordn, 'formaction' => $formaction, 'action' => $action]);
+		$paginator = new PaginatorBootstrap4($input->pageNum, $resultcount, $page->fullURL, $page->name);
+		$page->body = $config->twig->render('products/item.twig', ['page' => $page, 'page_title' => $page->title, 'items' => $items, 'search' => $search, 'q' => $q, 'ordn' => $ordn, 'formaction' => $formaction, 'action' => $action, 'paginator' => $paginator]);
 		include __DIR__ . "/basic-page.php";
 	}
