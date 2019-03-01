345f34cdd7f65227d80a46ea84426a709eff7b05
bbullemer <barbara.bullemer@gmail.com>
2019-03-01 14:05:58 -0600

    Edit-order Twig
    - added edit order twig file

--- site/templates/edit-order.php
+++ site/templates/edit-order.php
@@ -5,6 +5,9 @@
 	$order = $editorderdisplay->get_order();
 	$page->title = "Editing Order # $ordn for ".Customer::get_customernamefromid($order->custid);
 
-	$page->body = $config->twig->render('orders/edit-order.twig', ['page' => $page, 'page_title' => $page->title, 'editorderdisplay' => $editorderdisplay, 'ordn' => $ordn, 'order' => $order]);
+	$searchitems = $pages->get('/products/search/')->url.'?ordn=';
+	$ordersredir = $config->pages->orders.'redir/';
+
+	$page->body = $config->twig->render('orders/edit-order.twig', ['page' => $page, 'page_title' => $page->title, 'editorderdisplay' => $editorderdisplay, 'ordn' => $ordn, 'order' => $order, 'searchitems' => $searchitems, 'ordersredir' => $ordersredir]);
 	include __DIR__ . "/basic-page.php";
 ?>
