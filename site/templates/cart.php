49c9ce7a07652bc21d2a408e6f13d6dd68c8a21c
bbullemer <barbara.bullemer@gmail.com>
2019-03-04 17:35:18 -0600

    Cart and Customer Search Twig Conversion
    - added search-form.twig file
    - attempting to convert customer search from cart

--- site/templates/cart.php
+++ site/templates/cart.php
@@ -13,14 +13,15 @@
         $create_salesorder = $formaction. '?action=create-sales-order';
 
 		$page->title = "Cart for ".Customer::get_customernamefromid($cart->custid);
-        // $page->body = "{$config->paths->content}cart/cart-outline.php";
+
         $page->body = $config->twig->render('cart/cart-outline.twig', ['page' => $page, 'cartdisplay' => $cartdisplay, 'details' => $details, 'formaction' => $formaction, 'search' => $search, 'customers' => $customers, 'delete_line' => $delete_line, 'create_salesorder' => $create_salesorder, 'cart' => $cart]);
 		include __DIR__ . "/basic-page.php";
 	} else {
 		$input->get->function = 'cart';
 		$page->title = "Choose a Customer";
-		$page->body = "{$config->paths->content}customer/index/search-form.php";
-	}
+        $customersearch = $pages->get('/customers/')->url;
 
-	include('./_include-page.php');
+		$page->body = $config->twig->render('customer/search-form.twig', ['page_title' => $page->title, 'customersearch' => $customersearch, 'function' => $input->get->function]);
+        include __DIR__ . "/basic-page.php";
+	}
 ?>
--- site/templates/customer-index.php
+++ site/templates/customer-index.php
@@ -1,9 +1,14 @@
-<?php 
+<?php
     $page->title = "Choose a Customer";
-    $page->body = $config->paths->content."customer/index/search-form.php";
-    
+    // $page->body = $config->paths->content."customer/index/search-form.php";
+    $customersearch = $pages->get('/customers/')->url;
+    $function = $input->get->function;
+
+    $page->body = $config->twig->render('customer/search-form.twig', ['page_title' => $page->title, 'customersearch' => $customersearch, 'function' => $function]);
+
     if ($config->ajax) {
         include($page->body);
     } else {
-        include("./_include-page.php");
+        // include("./_include-page.php");
+        include __DIR__ . "/basic-page.php";
     }
