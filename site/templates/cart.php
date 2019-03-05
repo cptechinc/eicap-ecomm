3f6e36773160f7255ed66cc131daa4c88c8029cf
bbullemer <barbara.bullemer@gmail.com>
2019-03-05 13:22:37 -0600

    Customer Search
    - converted customer and cart for searching customers before cart has customer assigned

--- site/templates/cart.php
+++ site/templates/cart.php
@@ -1,27 +1,37 @@
 <?php
+    use Dplus\Dpluso\Customer\CustomerIndex;
+    use Dplus\Content\PaginatorBootstrap4;
+
     $cartdisplay = new Dplus\Dpluso\OrderDisplays\CartDisplay(session_id(), $page->fullURL, '#ajax-modal');
     $cart = $cartdisplay->get_cartquote();
 
 	if (!empty($cart->custid) && $cart->custid != $config->defaultcustid) {
         $details = get_cartdetails(session_id());
 
-        $search = $pages->get('/products/search/');
-        $customers = $pages->get('/customers/');
-
         $page->formaction = $config->pages->root.'cart/redir/';
-        $delete_line = $formaction. '?action=remove-line&line=';
-        $create_salesorder = $formaction. '?action=create-sales-order';
+        $delete_line = $page->formaction. '?action=remove-line&line=';
+        $create_salesorder = $page->formaction. '?action=create-sales-order';
 
 		$page->title = "Cart for ".Customer::get_customernamefromid($cart->custid);
 
-        $page->body = $config->twig->render('cart/cart-outline.twig', ['page' => $page, 'cartdisplay' => $cartdisplay, 'details' => $details, 'formaction' => $formaction, 'search' => $search, 'customers' => $customers, 'delete_line' => $delete_line, 'create_salesorder' => $create_salesorder, 'cart' => $cart]);
+        $page->body = $config->twig->render('cart/cart-outline.twig', ['page' => $page, 'pages' => $pages, 'cartdisplay' => $cartdisplay, 'details' => $details, 'delete_line' => $delete_line, 'create_salesorder' => $create_salesorder, 'cart' => $cart]);
 		include __DIR__ . "/basic-page.php";
 	} else {
-		$input->get->function = 'cart';
-		$page->title = "Choose a Customer";
+        $page->title = "Choose a Customer";
+        $q = $input->get->text('q');
         $customersearch = $pages->get('/customers/')->url;
+        $input->get->function = 'cart';
 
-		$page->body = $config->twig->render('customer/search-form.twig', ['page_title' => $page->title, 'customersearch' => $customersearch, 'function' => $input->get->function]);
+    	$pageurl = new Purl\Url($page->fullURL->getUrl());
+    	$pageurl->path = $config->pages->customer;
+    	$pageurl->query->set('function', 'cart');
+
+    	$custindex = new CustomerIndex($pageurl, '#cust-index-search-form', '#cust-index-search-form');
+    	$custindex->set_pagenbr($input->pageNum);
+    	$resultscount = $custindex->count_searchcustindex($q);
+    	$paginator = new PaginatorBootstrap4($custindex->pagenbr, $resultscount, $custindex->pageurl, 'customers', $custindex->ajaxdata);
+
+		$page->body = $config->twig->render('customer/search-form.twig', ['page' => $page, 'page_title' => $page->title, 'customersearch' => $customersearch, 'resultscount' => $resultscount, 'custindex' => $custindex, 'input' => $q, 'function' => $input->get->function, 'paginator' => $paginator]);
         include __DIR__ . "/basic-page.php";
 	}
 ?>
--- site/templates/customer-index.php
+++ site/templates/customer-index.php
@@ -1,14 +1,23 @@
 <?php
-    $page->title = "Choose a Customer";
-    // $page->body = $config->paths->content."customer/index/search-form.php";
-    $customersearch = $pages->get('/customers/')->url;
-    $function = $input->get->function;
+    use Dplus\Dpluso\Customer\CustomerIndex;
+    use Dplus\Content\PaginatorBootstrap4;
 
-    $page->body = $config->twig->render('customer/search-form.twig', ['page_title' => $page->title, 'customersearch' => $customersearch, 'function' => $function]);
+    $page->title = "Choose a Customer";
+	$q = $input->get->text('q');
+    $customersearch = $pages->get('/customers/')->url;
+
+    $pageurl = new Purl\Url($page->fullURL->getUrl());
+    $pageurl->path = $config->pages->customer;
+
+    $custindex = new CustomerIndex($pageurl, '#cust-index-search-form', '#cust-index-search-form');
+    $custindex->set_pagenbr($input->pageNum);
+    $resultscount = $custindex->count_searchcustindex($q);
+    $paginator = new PaginatorBootstrap4($custindex->pagenbr, $resultscount, $custindex->pageurl, 'customers', $custindex->ajaxdata);
+
+    $page->body = $config->twig->render('customer/search-form.twig', ['page' => $page, 'customersearch' => $customersearch, 'custindex' => $custindex, 'input' => $q, 'paginator' => $paginator]);
 
     if ($config->ajax) {
-        include($page->body);
+        echo $page->body;
     } else {
-        // include("./_include-page.php");
         include __DIR__ . "/basic-page.php";
     }
