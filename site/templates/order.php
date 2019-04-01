cd3ae4a76aa38fcf9b8cbe2456fafc53e6937b61
bbullemer <barbara.bullemer@gmail.com>
2019-04-01 10:06:24 -0500

    Combine Order and History-Order Templates
    - removed history-order template
    - added elseif statement to order.php to check for saleshistory
    - changed session->loc in orders/redir
    - changed home.twig for sales-history link template change
    - changed button link

--- site/templates/order.php
+++ site/templates/order.php
@@ -1,12 +1,11 @@
 <?php
     use Dplus\Base\DplusDateTime;
 
-
     if ($input->get->ordn) {
         $ordn = $input->get->text('ordn');
-        $orderdisplay = new Dplus\Dpluso\OrderDisplays\SalesOrderDisplay(session_id(), $page->fullURL, '', $ordn);
 
         if (SalesOrder::exists($ordn)) {
+            $orderdisplay = new Dplus\Dpluso\OrderDisplays\SalesOrderDisplay(session_id(), $page->fullURL, '', $ordn);
             $order = $orderdisplay->get_order();
             $customer = Customer::load($order->custid, $order->shiptoid);
             $page->title = "Order #$ordn for $customer->name";
@@ -14,6 +13,15 @@
             $dplusdatetime = new DplusDateTime();
 
             $page->body = $config->twig->render('orders/order.twig', ['page' => $page, 'user' => $user, 'orderdisplay' => $orderdisplay, 'dplusdatetime' => $dplusdatetime, 'order' => $order, 'customer' => $customer]);
+        } elseif (SalesOrderHistory::is_saleshistory($ordn)) {
+            $orderhistorydisplay = new Dplus\Ecomm\SalesOrderHistoryDisplay(session_id(), $page->fullURL, '', $ordn);
+            $order = $orderhistorydisplay->get_order();
+            $customer = Customer::load($order->custid, $order->shiptoid);
+            $page->title = "Order #$ordn for $customer->name";
+
+            $dplusdatetime = new DplusDateTime();
+
+            $page->body = $config->twig->render('sales-history/history-order.twig', ['page' => $page, 'orderhistorydisplay' => $orderhistorydisplay, 'dplusdatetime' => $dplusdatetime, 'order' => $order, 'customer' => $customer]);
         } else {
             $page->title = "Error: Order #$ordn could not be loaded";
             $msg = "Order #$ordn may not exist or an error occured during loading";
