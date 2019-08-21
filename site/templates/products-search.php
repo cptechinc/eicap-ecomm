85473c172043d5ffb260e9c1a74a0c04bfa7406a
Paul Gomez <pauldro952@outlook.com>
2019-08-21 09:16:43 -0500

    f

--- site/templates/products-search.php
+++ site/templates/products-search.php
@@ -1,48 +1,29 @@
 <?php
-	use Dplus\Content\PaginatorBootstrap4;
-
 	$q = $input->get->text('q');
 	$page->title = $input->get->q ? "Searching for '$q'" : "Search Products";
 
 	// Processwire Selectors
 	// https://processwire.com/api/selectors/
 	$selector = "template=product|imitem, title|body|itemid|name1|name2%=$q";
-	$limit = 10;
-	$start = $input->pageNum > 1 ? $input->pageNum * $limit : 0;
-	$selector .= ", limit=$limit, start=$start";
-	$resultcount = $pages->count($selector);
 
-	$items = $pages->find($selector);
-
-	if ($input->get->ordn) {
-		$page->formaction = "{$config->pages->orders}redir/";
-		$page->ordn = $input->get->text('ordn');
-		$page->action = "add-to-order";
-	} else {
-		$page->formaction = "{$config->pages->cart}redir/";
-		$page->action = "add-to-cart";
-		$page->ordn = false;
-	}
-
-	if ($input->get->page) {
-		$returnurl = $input->get->text('page');
-	} else {
-		$returnurl = $page->url;
-	}
+	$logmuser = LogmUser::load($user->loginid);
+	$selector .= ", famID=$config->warehouse_all|$logmuser->whseid";
 
 	if ($config->ajax) {
-		$ajax = 'data-loadinto=".modal-content" data-focus=".modal-content"';
-		$paginator = new PaginatorBootstrap4($input->pageNum, $resultcount, $page->fullURL, $page->name, $ajax);
+		$limit = 10;
+		$start = $input->pageNum > 1 ? $input->pageNum * $limit : 0;
+		$selector .= ", limit=$limit, start=$start";
+	}
 
-		$modal_id = 'item-search';
-		// $page->body = $config->paths->content."products/search/results-ajax.php";
-		$page->body = $config->twig->render('products/item-ajax.twig', ['page' => $page, 'pages' => $pages, 'items' => $items, 'modal_id' => $modal_id, 'returnurl' => $returnurl, 'paginator' => $paginator]);
+	$resultcount = $pages->count($selector);
+	$products = $pages->find($selector);
+
+	if ($config->ajax) {
+		$page->body = $config->paths->content."products/search/results-ajax.php";
 		if ($config->modal) {
-			// include('./_include-ajax-modal.php');
-			include __DIR__ . "/_include-ajax-modal.php";
+			include('./_include-ajax-modal.php');
 		}
 	} else {
-		$paginator = new PaginatorBootstrap4($input->pageNum, $resultcount, $page->fullURL, $page->name);
-		$page->body = $config->twig->render('products/item-list.twig', ['page' => $page, 'pages' => $pages, 'items' => $items, 'q' => $q, 'paginator' => $paginator]);
-		include __DIR__ . "/basic-page.php";
+		$page->body = $config->paths->content."products/search/results.php";
+		include('./_include-page.php');
 	}
